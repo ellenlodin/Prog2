fc77476df582ecd47edcad8c8eebd88f4da88ef3
Ellen Lodin <ellen.lodin@telia.com>
2024-10-22 20:55:59 +0200

    updated VA4

--- VA_files/VA_4.py
+++ VA_files/VA_4.py
@@ -1,42 +1,56 @@
 """
 Solutions to module VA 4
 
-Student:
-Mail:
+Student: Ellen Lodin
+Mail: ellen.lodin@telia.com
 """
-#!/usr/bin/env python3
 
-from person import Person
-"""
-Write a script that gives a plot for comparison of two approaches for Fibonacci numbers
-"""
-#Python
+from time import perf_counter as pc
+import matplotlib.pyplot as plt
+from numba import njit
+import numpy as np
+
 def fib_py(n):
-	if n <= 1:
-		return n
-	else:
-		return(fib_py(n-1) + fib_py(n-2))
+    if n <= 1:
+        return n
+    else:
+        return (fib_py(n-1) + fib_py(n-2))
 
 @njit
 def fib_numba(n):
 	if n <= 1:
 		return n
 	else:
-		return(fib_py(n-1) + fib_py(n-2))
+		return(fib_numba(n-1) + fib_numba(n-2))
 
-def main():
-	f = Person(50)
-	print(f.getAge())
-	print(f.getDecades())
+n1 = np.linspace(20, 30)
 
-	f.setAge(51)
-	print(f.getAge())
-	print(f.getDecades())
+t_py = []
+t_numba = []
 
-	print(fib_py(47))
-	print(fib_numba(47))
+fib_res_py = fib_py(47)
+fib_res_numba = fib_numba(47)
 
-if __name__ == '__main__':
-	main()
+print(f'Python: fib(47)= {fib_res_py}')
+print(f'Numba: fib(47)= {fib_res_numba}')
 
-"""What is the result for Fibonacci with n=47? Why?"""
+for i in n1: 
+	start1 = pc()
+	fib_py(i)
+	stop1 = pc()
+	t_py.append(stop1-start1)
+	
+for i in n1: 
+	start2 = pc()
+	fib_numba(i)
+	stop2 = pc()
+	t_numba.append(stop2-start2)
+	
+plt.plot(n1, t_py, label='Python processing time', color='blue', marker='o')     
+plt.plot(n1, t_numba, label='Python-Numba processing time', color='green', marker='s')  
+plt.xlabel('n')
+plt.ylabel('t (s)')
+plt.title('Fibonacci Execution Time: Python vs Numba')
+plt.legend()
+plt.savefig('Fibonacci Execution Time - Python vs Numba.png')
+plt.show()
